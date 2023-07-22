3be674a4c4e0b7fe9a9afe318890097e736bb24c
SanTy <89365439+SanTysss1984@users.noreply.github.com>
2023-07-22 16:04:24 +0200

    Add files via upload

---
+++ snt-gardencleaner/config/translations.lua
@@ -0,0 +1,30 @@
+--████████╗██████╗░░█████╗░███╗░░██╗░██████╗██╗░░░░░░█████╗░████████╗██╗░█████╗░███╗░░██╗░██████╗
+--╚══██╔══╝██╔══██╗██╔══██╗████╗░██║██╔════╝██║░░░░░██╔══██╗╚══██╔══╝██║██╔══██╗████╗░██║██╔════╝
+--░░░██║░░░██████╔╝███████║██╔██╗██║╚█████╗░██║░░░░░███████║░░░██║░░░██║██║░░██║██╔██╗██║╚█████╗░
+--░░░██║░░░██╔══██╗██╔══██║██║╚████║░╚═══██╗██║░░░░░██╔══██║░░░██║░░░██║██║░░██║██║╚████║░╚═══██╗
+--░░░██║░░░██║░░██║██║░░██║██║░╚███║██████╔╝███████╗██║░░██║░░░██║░░░██║╚█████╔╝██║░╚███║██████╔╝
+--░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═════╝░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░╚════╝░╚═╝░░╚══╝╚═════╝░
+
+Config.Languages = { --You can modify your language or even create new languages ​​here.
+    ['en'] = {
+        ["BLIP_NAME"] = "Garden Cleaner Job",
+        ["BLIP_HOUSE"] = "House To Clean",
+        ["TARGET_STARTJOB"] = "Start/Finish Job",
+        ["TARGET_PUTONCLOTHES"] = "Put clothes on",
+        ["TAKE_OUT_CAR"] = "[E] Vybrat von auto",
+        ["TAKE_IN_CAR"] = "[E] Ulozit auto",
+        ["PARK_THE_CAR"] = "[E] Zaparkovat auto",
+        ["TARGET_START_CHARVEST"] = "Vytiahnut rastlinu",
+        ["TARGET_START_BINBAGS"] = "Zobrat odpatky",
+        ["TARGET_PUTINBIN"] = "Hodit odpatky do kosa",
+        ["TARGET_START_LEAFS"] = "Odfukat listie",
+        ["INFO_DONE"] = "Done: ",
+        ["INFO_FIRST_FINISH_JOB"] = "Ak chcete zastavit pracu, musite ju dokoncit!",
+        ["INFO_BIN"] = "Najdite blizke odpadky a vyhodte ich",
+        ["INFO_DONE_JOB"] = "Praca dokoncena, mozete ist vratit auto na firmu",
+        ["JOB_FINISH_GIVE_ME_THE_FUCKING_MONEY"] = "Zarobil si: $",
+        -- TALK --
+        ['TALK_MIKE_GOTOPLACE'] = "[SanTy] Hej, choď na miesto označené na mape, tam ti poviem viac more dik",
+        ['TALK_MIKE_INPLACE'] = "[SanTy] Ó, si tam teraz! Choď k žltým značkám a komunikujte s nimi! cez ALT !!!",
+    }
+}
